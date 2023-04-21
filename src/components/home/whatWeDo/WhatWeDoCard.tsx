f76b92d327d77f80c3e89a852bd3187f1fabfd21
DangVanHy <hy83512@st.vimaru.edu.vn>
2023-04-21 10:20:19 +0700

    fix: width WhatWeDoCard
    (cherry picked from commit 66063c496141e4420fd98a864d8d136f1c54b6fa)

--- src/components/home/whatWeDo/WhatWeDoCard.tsx
+++ src/components/home/whatWeDo/WhatWeDoCard.tsx
@@ -15,15 +15,15 @@ export function WhatWeDoCard({
         <div
             className={`card scroll-mx-[25px] ${className}`}
         >
-            <div className="image w-full flex justify-center overflow-hidden">
+            <div className="image w-[calc(100vw-50px)] sm:w-[310px] max-w-[400px] flex justify-center overflow-hidden">
                 <img src={imageUrl} alt="" className="image-mask w-[230px] h-full object-cover" />
             </div>
 
             <div className="pt-[26px] w-full items-center justify-center flex flex-col">
-                <div className="text-[30px] leading-[32px] text-center w-[310px] h-[64px] font-bold">
+                <div className="text-[30px] leading-[32px] sm:w-[310px] max-w-[400px] text-center w-[calc(100vw-50px)] h-[64px] font-bold">
                     {title}
                 </div>
-                <div className="text-[16px] leading-[24px] w-[310px] tracking-[0.15px] text-center pt-[42px]">
+                <div className="text-[16px] leading-[24px] sm:w-[310px] max-w-[400px] w-[calc(100vw-50px)] tracking-[0.15px] text-center pt-[42px]">
                     {children}
                 </div>
             </div>
