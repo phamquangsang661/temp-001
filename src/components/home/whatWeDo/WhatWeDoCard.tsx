e773134ff513e17f02acbc5055e7b53559d3b93c
DangVanHy <hy83512@st.vimaru.edu.vn>
2023-04-21 08:34:59 +0700

    fix: WhatWeDo interface
    (cherry picked from commit 28123f709de415b740f81e89d4089e382c67db32)

--- src/components/home/whatWeDo/WhatWeDoCard.tsx
+++ src/components/home/whatWeDo/WhatWeDoCard.tsx
@@ -13,14 +13,14 @@ export function WhatWeDoCard({
 }: WhatWeDoCard) {
     return (
         <div
-            className={`card scroll-mx-[32px] items-center justify-center flex flex-col w-[310px] ${className}`}
+            className={`card scroll-mx-[40px] ${className}`}
         >
             <div className="image w-full flex justify-center overflow-hidden">
                 <img src={imageUrl} alt="" className="image-mask w-[230px] h-full object-cover" />
             </div>
 
             <div className="pt-[26px] w-full items-center justify-center flex flex-col">
-                <div className="text-[30px] leading-[32px] text-center w-[280px] h-[64px] font-bold">
+                <div className="text-[30px] leading-[32px] text-center w-[310px] h-[64px] font-bold">
                     {title}
                 </div>
                 <div className="text-[16px] leading-[24px] w-[310px] tracking-[0.15px] text-center pt-[42px]">
